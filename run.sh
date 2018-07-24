11d57a66e8d142c60c11fce474d20a3e6e2538bd
Everton Gonçalves <dsgeverton@gmail.com>
2018-07-24 13:37:07 -0300

    Add shell script

---
+++ run.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+# Variables
+opt=""
+ipServer=""
+serverPort="35777" # If not set, starting with default value
+nickname="anonymous"
+
+
+run () {
+  printf "Starting server... "
+  sleep 3 && printf "OK!\n" && sleep 1
+  echo "+===========================+"
+  echo "|  TERMINAL SERVER STARTED  |"
+  echo "+===========================+"
+  echo ""
+  echo " - Press 'Ctrl + C' to exit."
+  echo ""
+  mawk -W interactive '$0="'${nickname}': " $0' | nc -l -p ${serverPort} ${ipServer}
+}
+
+menu () {
+  while true
+  do
+  clear
+  echo "================================================"
+  echo "A Simple chat using terminal"
+  echo "Builded for: Everton Gonçalves"
+  echo "Github: @dsgeverton"
+  echo ""
+  echo "1) New Server Chat"
+  echo ""
+  echo "2) Sign In a Chat"
+  echo ""
+  echo "0) Exit"
+  echo ""
+  echo "================================================"
+
+  echo "Option: "
+  read opt
+  echo "Setted option: ($opt)"
+  echo "================================================"
+
+  case "$opt" in
+
+      1)
+        # echo "Trying get your IP address..."
+        # sip="/tmp/sip"
+        # aux="/tmp/aux"
+        # ifconfig > $sip
+        # cat $sip | awk 'NR>=2 && NR<=2' > $aux
+        # show="`cat $aux | awk '{print $3}'`"
+        # rm $aux |rm $sip
+        # echo "$show"
+        echo "Entry your IP address: "
+        read ipServer
+
+        echo "Entry your nickname: "
+        read nickname
+        run
+
+  echo "================================================"
+  ;;
+      2)
+
+  echo "================================================"
+  ;;
+      0)
+        echo "Exiting..."
+        sleep 3
+        clear;
+        exit;
+  echo "================================================"
+  ;;
+
+  *)
+        echo "Invalid Option!"
+esac
+done
+
+}
+menu
